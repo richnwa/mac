6f54b9922b7dd4b759d465a903a5ebc2e07e391f
richnwa <rich.nwa.unit@gmail.com>
2021-03-04 20:36:08 +0100

    all files

---
+++ mac/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div>
+		<ul>
+			<h1>Hi</h1>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+		</ul>
+	</div>
+	<div>NEW</div>
+</body>
+</html>
